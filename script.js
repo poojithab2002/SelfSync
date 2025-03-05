b20bf4dfa8a88dba7105834157a18a87ea4dda10
poojithab2002 <poojithabagam27@gmail.com>
2025-03-04 19:21:02 -0500

    Added basic JavaScript interactivity for Get Started button

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the "Get Started" button
+const getStartedButton = document.querySelector("button");
+
+// Add a click event listener
+getStartedButton.addEventListener("click", function () {
+    alert("Welcome to SelfSync! Let's start tracking your habits.");
+});
